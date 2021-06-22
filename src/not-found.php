ff046587118b9ff19b6d2cfe3ccd8fb1c79e5d85
Eric Mackrodt <eric.mackrodt@safetyculture.io>
2021-06-22 12:04:26 +1000

    Page not found.

---
+++ src/not-found.php
@@ -0,0 +1,8 @@
+<?php $this->layout(withVariant('layout'), ['title' => 'Page not found']) ?>
+
+<div class="content">
+  <div id="not-found">
+    <h1>Page not found</h1>
+    <p>The page you're trying to access does not exist =(</p>
+  </div>
+</div>
